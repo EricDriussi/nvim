387b8a9b12aeb075a4532f8ad14e859f0b504456
Eric Driussi <ericdriussi@gmail.com>
2022-06-16 17:27:48 +0100

    update and format

--- lua/plugins-config/null-ls.lua
+++ lua/plugins-config/null-ls.lua
@@ -1,34 +1,30 @@
-local null_ls = require("null-ls")
---local null_ls_status_ok, null_ls = pcall(require, "null-ls")
---if not null_ls_status_ok then
---return
---end
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+  return
+end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local actions = null_ls.builtins.code_actions
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.stylua.with({
-                    timeout = 10000
-                }),
-		formatting.eslint.with({
-                    timeout = 10000
-                }),
+  debug = false,
+  sources = {
+    formatting.eslint.with({
+      timeout = 10000,
+    }),
 
-		--diagnostics.eslint,
-                diagnostics.yamllint,
+    --diagnostics.eslint,
+    diagnostics.yamllint,
 
-		-- CSS
-		formatting.stylelint,
-		diagnostics.stylelint,
+    -- CSS
+    formatting.stylelint,
+    diagnostics.stylelint,
 
-		-- LSP for shell sucks a bit
-		formatting.shellharden,
-		diagnostics.shellcheck,
-                actions.shellcheck,
+    -- LSP for shell sucks a bit
+    formatting.shellharden,
+    diagnostics.shellcheck,
+    actions.shellcheck,
 
-                formatting.black,
-	},
+    formatting.black,
+  },
 })
