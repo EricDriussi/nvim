62f646aa1e1cf1dcf741edca5647e3948d7cadf2
Eric Driussi <ericdriussi@gmail.com>
2022-06-18 18:22:51 +0100

    formatting

--- lua/plugins-config/lsp/settings/zk.lua
+++ lua/plugins-config/lsp/settings/zk.lua
@@ -1,12 +1,12 @@
 local util = require 'lspconfig.util'
 
 return {
-    settings = {
-        zk = {
-            --root_dir = util.root_pattern(".gitignore")
-            root_dir = function()
-                return util.root_pattern(".gitignore") or vim.loop.cwd()
-            end;
-        }
+  settings = {
+    zk = {
+      --root_dir = util.root_pattern(".gitignore")
+      root_dir = function()
+        return util.root_pattern(".gitignore") or vim.loop.cwd()
+      end;
     }
+  }
 }
