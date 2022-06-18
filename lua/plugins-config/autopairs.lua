ba48295b8b0f16aac3ffcae1b6edd72077f6d8a3
Eric Driussi <ericdriussi@gmail.com>
2022-06-18 18:49:54 +0100

    refactor autopairs

--- lua/plugins-config/autopairs.lua
+++ lua/plugins-config/autopairs.lua
@@ -1,6 +1,10 @@
-local npairs = require('nvim-autopairs')
+local status_ok, npairs = pcall(require, "nvim-autopairs")
+if not status_ok then
+  return
+end
 
 npairs.setup {
+  -- Use TreeSitter
   check_ts = true,
   ts_config = {
     lua = { "string", "source" },
@@ -8,17 +12,6 @@ npairs.setup {
     java = false,
   },
   disable_filetype = { "TelescopePrompt", "spectre_panel" },
-  fast_wrap = {
-    map = "<M-e>",
-    chars = { "{", "[", "(", '"', "'" },
-    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-    offset = 0, -- Offset from pattern match
-    end_key = "$",
-    keys = "qwertyuiopzxcvbnmasdfghjkl",
-    check_comma = true,
-    highlight = "PmenuSel",
-    highlight_grey = "LineNr",
-  },
   enable_check_bracket_line = false
 }
 
@@ -31,59 +24,58 @@ end
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
 
 -- Custome Rules!!
-local Rule = require('nvim-autopairs.rule')
-local cond = require('nvim-autopairs.conds')
+local custom_rule = require('nvim-autopairs.rule')
+local except = require('nvim-autopairs.conds')
+
 npairs.add_rules({
+  -- MD bold and italics
+  custom_rule("*", "*", { "md", "markdown" })
+      :with_pair(except.not_after_regex("[%w%.]"))
+      :with_pair(except.not_before_regex("[%w]")),
+  custom_rule("`", "`")
+      :with_pair(except.not_after_regex("[%w%.]"))
+      :with_pair(except.not_before_regex("[%w]")),
 
--- MD bold and italics
-  Rule("*", "*",{"md", "markdown"})
-    :with_pair(cond.not_after_regex("[%w%.]"))
-    :with_pair(cond.not_before_regex("[%w]")),
-  Rule("`", "`")
-    :with_pair(cond.not_after_regex("[%w%.]"))
-    :with_pair(cond.not_before_regex("[%w]")),
+  -- Spanish
+  custom_rule("¿", "?")
+      :with_pair(except.not_after_regex("[%w%.]"))
+      :with_pair(except.not_before_regex("[%w%.]")),
+  custom_rule("¡", "!")
+      :with_pair(except.not_after_regex("[%w%.]"))
+      :with_pair(except.not_before_regex("[%w%.]")),
 
--- Spanish
-  Rule("¿", "?")
-    :with_pair(cond.not_after_regex("[%w%.]"))
-    :with_pair(cond.not_before_regex("[%w%.]")),
-  Rule("¡", "!")
-    :with_pair(cond.not_after_regex("[%w%.]"))
-    :with_pair(cond.not_before_regex("[%w%.]")),
+  -- js arrow function
+  custom_rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript' })
+      :use_regex(true)
+      :set_end_pair_length(2),
 
--- js arrow function
-  Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript' })
-    :use_regex(true)
-    :set_end_pair_length(2),
-
--- spacing for = sign
-  Rule('=', '', {'-vim', '-zsh', '-env'})
-    :with_pair(cond.not_inside_quote())
-    :with_pair(function(opts)
+  -- spacing for = sign
+  custom_rule('=', '', { '-vim', '-zsh', '-env' })
+      :with_pair(except.not_inside_quote())
+      :with_pair(function(opts)
         local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
         if last_char:match('[%w%=%s]') then
-            return true
+          return true
         end
         return false
-    end)
-    :replace_endpair(function(opts)
+      end)
+      :replace_endpair(function(opts)
         local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
         local next_char = opts.line:sub(opts.col, opts.col)
         next_char = next_char == ' ' and '' or ' '
         if prev_2char:match('%w$') then
-            return '<bs> =' .. next_char
+          return '<bs> =' .. next_char
         end
         if prev_2char:match('%=$') then
-            return next_char
+          return next_char
         end
         if prev_2char:match('=') then
-            return '<bs><bs>=' .. next_char
+          return '<bs><bs>=' .. next_char
         end
         return ''
-    end)
-    :set_end_pair_length(0)
-    :with_move(cond.none())
-    :with_del(cond.none())
-  }
+      end)
+      :set_end_pair_length(0)
+      :with_move(except.none())
+      :with_del(except.none())
+}
 )
-
