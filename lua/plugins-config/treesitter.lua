a28e14b13edf7b03b16f2c098d76b249a62f2eef
Eric Driussi <ericdriussi@gmail.com>
2022-06-18 19:38:50 +0100

    refactor treesitter

--- lua/plugins-config/treesitter.lua
+++ lua/plugins-config/treesitter.lua
@@ -1,36 +1,29 @@
-require'nvim-treesitter.configs'.setup {
-    -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = "all",
+local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
 
-    -- Install languages synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+treesitter.setup {
+  ensure_installed = "all",
+  sync_install = false,
+  highlight = {
+    enable = true,
+  },
+  indent = { enable = true, disable = { "yaml" } },
 
-    -- List of parsers to ignore installing
-    ignore_install = { "" },
-    highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-        -- list of language that will be disabled
-        disable = {""},
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true, disable = { "yaml" } },
-    rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
-      },
-    autopairs = {
-        enable = true,
-    },
-    autotag = {
-        enable = true,
-    },
+  -- Plugins!
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }
+    extended_mode = true,
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  },
+  autopairs = {
+    enable = true,
+  },
+  autotag = {
+    enable = true,
+  },
 }
