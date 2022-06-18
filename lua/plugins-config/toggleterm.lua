673dafeafb271419f8c1bfc97c91e50895be89ae
Eric Driussi <ericdriussi@gmail.com>
2022-06-18 19:33:56 +0100

    refactor toggleterm

--- lua/plugins-config/toggleterm.lua
+++ lua/plugins-config/toggleterm.lua
@@ -1,49 +1,46 @@
-require("toggleterm").setup({
-	size = 20,
-	open_mapping = [[<a-t>]],
-	hide_numbers = true,
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = 2,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	direction = "float",
-	close_on_exit = true,
-	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-		winblend = 0,
-		highlights = {
-			border = "Normal",
-			background = "Normal",
-		},
-	},
+local status_ok, toggleterm = pcall(require, "toggleterm")
+if not status_ok then
+  return
+end
+
+toggleterm.setup({
+  size = 20,
+  open_mapping = [[<a-t>]],
+  hide_numbers = true,
+  shade_filetypes = {},
+  shade_terminals = true,
+  shading_factor = 2,
+  start_in_insert = true,
+  insert_mappings = true,
+  persist_size = true,
+  direction = "float",
+  close_on_exit = true,
+  shell = vim.o.shell,
+  float_opts = {
+    border = "curved",
+    winblend = 0,
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    },
+  },
 })
 
 function _G.set_terminal_keymaps()
-	local opts = { noremap = true }
-	vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+  local opts = { noremap = true }
+  vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
 end
 
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 local Terminal = require("toggleterm.terminal").Terminal
 
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
 function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
+  local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+  lazygit:toggle()
 end
-
-local node = Terminal:new({ cmd = "node", hidden = true })
 
 function _NODE_TOGGLE()
-	node:toggle()
-end
-
-local sml = Terminal:new({ cmd = "sml", hidden = true })
-
-function _SML_TOGGLE()
-	sml:toggle()
+  local node = Terminal:new({ cmd = "node", hidden = true })
+  node:toggle()
 end
