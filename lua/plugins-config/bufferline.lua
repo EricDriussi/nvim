9d626cb9b1252c20910b96640166543d81055ca0
Eric Driussi <ericdriussi@gmail.com>
2022-06-18 18:52:14 +0100

    refactor bufferline

--- lua/plugins-config/bufferline.lua
+++ lua/plugins-config/bufferline.lua
@@ -1,10 +1,18 @@
-require('bufferline').setup {
+local status_ok, bufferline = pcall(require, "bufferline")
+if not status_ok then
+  return
+end
+
+bufferline.setup {
   options = {
+    -- Tab like usual
     mode = "tabs",
+    -- Show errors on tabs
     diagnostics = "nvim_lsp",
     diagnostics_indicator = function()
       return " ⚠️"
     end,
+    -- Offset file explorer
     offsets = {
       {
         filetype = "NvimTree",
