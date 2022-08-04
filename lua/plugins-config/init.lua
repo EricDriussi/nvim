cd9d65b436ead6fe94afb4677fdfcd024e3ccbda
Eric Driussi <ericdriussi@gmail.com>
2022-08-04 18:25:26 +0100

    add highlight on yank

--- lua/plugins-config/init.lua
+++ lua/plugins-config/init.lua
@@ -41,3 +41,19 @@ require("harpoon").setup({ menu = { width = vim.api.nvim_win_get_width(0) - 70 }
 require("pqf").setup()
 -- Color hex codes
 require("colorizer").setup()
+
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'TermCursor',
+      timeout = 40,
+    })
+  end,
+})
