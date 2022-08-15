3331437cb07a18cccc9893e78938f73b1177f181
Eric Driussi <ericdriussi@gmail.com>
2022-08-15 18:45:46 +0100

    fix autocmds

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,38 +1,26 @@
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-
-local yank_group = augroup('HighlightYank', {})
-autocmd('TextYankPost', {
+local yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
+vim.api.nvim_create_autocmd('TextYankPost', {
   group = yank_group,
   pattern = '*',
   callback = function()
     vim.highlight.on_yank({
       higroup = 'TermCursor',
-      timeout = 40,
+      timeout = 60,
     })
   end,
 })
 
 Module = {}
 function Module.activate_lsp_highlight()
-  local lsp_highlight_group = augroup('LspHighlight', {})
-  autocmd('CursorHold', {
+  local lsp_highlight_group = vim.api.nvim_create_augroup('LspHighlight', {})
+  vim.api.nvim_create_autocmd('CursorHold', {
     group = lsp_highlight_group,
-    pattern = '<buffer>',
     callback = function()
       vim.lsp.buf.document_highlight()
     end,
   })
-  autocmd('CursorHoldI', {
+  vim.api.nvim_create_autocmd('CursorMoved', {
     group = lsp_highlight_group,
-    pattern = '<buffer>',
-    callback = function()
-      vim.lsp.buf.document_highlight()
-    end,
-  })
-  autocmd('CursorMoved', {
-    group = lsp_highlight_group,
-    pattern = '<buffer>',
     callback = function()
       vim.lsp.buf.clear_references()
     end,
