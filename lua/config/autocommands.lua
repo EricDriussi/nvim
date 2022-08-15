57e916a0272fd304d8c5dae87aa90d40ab25373f
Eric Driussi <ericdriussi@gmail.com>
2022-08-15 19:22:55 +0100

    update format on save

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -13,17 +13,26 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 Module = {}
 function Module.activate_lsp_highlight()
   local lsp_highlight_group = vim.api.nvim_create_augroup('LspHighlight', {})
-  vim.api.nvim_create_autocmd('CursorHold', {
+  vim.api.nvim_create_autocmd('CursorHold, CursorHoldI', {
     group = lsp_highlight_group,
     callback = function()
       vim.lsp.buf.document_highlight()
     end,
   })
-  vim.api.nvim_create_autocmd('CursorMoved', {
+  vim.api.nvim_create_autocmd('CursorMoved, CursorMovedI', {
     group = lsp_highlight_group,
     callback = function()
       vim.lsp.buf.clear_references()
     end,
+  })
+end
+
+function Module.create_format_on_save_cmd()
+  return vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*',
+    callback = function()
+      vim.lsp.buf.formatting_sync()
+    end
   })
 end
 
--- lua/config/format-toggle.lua
+++ lua/config/format-toggle.lua
@@ -1,19 +1,12 @@
-Command_to_toggle = {
-  event = 'BufWritePre', opts = {
-    pattern = '*',
-    command = 'lua vim.lsp.buf.formatting_sync()'
-  }
-}
-
-Format_on_save_cmd_id = vim.api.nvim_create_autocmd(Command_to_toggle.event, Command_to_toggle.opts)
+local format_on_save_cmd_id = require("config.autocommands").create_format_on_save_cmd()
 
 function ToggleFormatOnSave()
-  if Format_on_save_cmd_id ~= 0 then
-    vim.api.nvim_del_autocmd(Format_on_save_cmd_id)
-    Format_on_save_cmd_id = 0
+  if format_on_save_cmd_id ~= 0 then
+    vim.api.nvim_del_autocmd(format_on_save_cmd_id)
+    format_on_save_cmd_id = 0
     print('Format On Save OFF')
   else
-    Format_on_save_cmd_id = vim.api.nvim_create_autocmd(Command_to_toggle.event, Command_to_toggle.opts)
+    format_on_save_cmd_id = require("config.autocommands").create_format_on_save_cmd()
     print('Format On Save ON')
   end
 end
