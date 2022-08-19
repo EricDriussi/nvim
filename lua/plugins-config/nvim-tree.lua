79d9a925710585f8708ba0d0eb0c0cca4dddff32
Eric Driussi <ericdriussi@gmail.com>
2022-08-19 17:53:29 +0100

    refactor nvim-tree

--- lua/plugins-config/nvim-tree.lua
+++ lua/plugins-config/nvim-tree.lua
@@ -2,107 +2,35 @@ require("nvim-tree").setup {
   disable_netrw = true,
 
   -- Show errors in files
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
+  diagnostics = { enable = true },
 
   -- Follow open file
-  sync_root_with_cwd = true,
   respect_buf_cwd = true,
   update_focused_file = {
     enable = true,
     update_root = true,
   },
 
-  -- What to show (or not)
-  filters = {
-    dotfiles = true,
-    custom = { '^\\.git' }
-  },
+  filters = { custom = { '^\\.git' } },
 
-  -- Behavior
-  actions = {
-    open_file = {
-      quit_on_open = false,
-      window_picker = {
-        enable = true
-      }
-    }
-  },
-
-  -- Location & Dimentions
   view = {
+    -- Adapt size to names being shown
     adaptive_size = true,
-    width = 30,
-    height = 30,
     hide_root_folder = true,
-    side = "left",
 
-    -- Mappings
     mappings = {
       list = {
-        { key = { "l", "<CR>", "o" }, action = "edit" },
+        { key = { "l", "<CR>", "o", "e" }, action = "edit" },
         { key = "h", action = "close_node" },
         { key = "<C-h>", action = "split" },
-        { key = "<C-v>", action = "vsplit" },
-        { key = "<C-t>", action = "tabnew" },
-        { key = "a", action = "create" },
-        { key = "dd", action = "remove" },
-        { key = "D", action = "trash" },
-        { key = "r", action = "rename" },
         { key = "R", action = "full_rename" },
-        { key = "x", action = "cut" },
-        { key = "p", action = "paste" },
         { key = "yf", action = "copy" },
         { key = "yn", action = "copy_name" },
         { key = "yp", action = "copy_path" },
-        { key = "H", action = "toggle_dotfiles" },
         { key = "?", action = "toggle_help" },
         { key = "-", action = "" },
         { key = "y", action = "" },
       },
     },
-  },
-
-  -- UI
-  renderer = {
-    root_folder_modifier = ":t",
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-        folder = true,
-        file = true,
-        folder_arrow = true,
-      },
-
-      -- Icons
-      glyphs = {
-        default = "",
-        symlink = "",
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          deleted = "",
-          untracked = "U",
-          ignored = "◌",
-        },
-        folder = {
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-        },
-      }
-    }
   },
 }
