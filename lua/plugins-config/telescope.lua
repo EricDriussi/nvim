4dc428e991f84c43a30bba205a7d081fafe65118
Eric Driussi <ericdriussi@gmail.com>
2022-08-19 18:49:44 +0100

    refactor telescope

--- lua/plugins-config/telescope.lua
+++ lua/plugins-config/telescope.lua
@@ -1,16 +1,5 @@
 local telescope = require("telescope")
 local actions = require("telescope.actions")
-local config = require("telescope.config")
-
-local find_files_command = { "rg", "--hidden", "--files", "--glob", "!.git" }
-local find_strings_command = { "rg", "--hidden", "--glob", "!.git/*" }
-
-local default_vimgrep_args = { unpack(config.values.vimgrep_arguments) }
-local updated_vimgrep_args = vim.tbl_deep_extend(
-  'force',
-  default_vimgrep_args,
-  find_strings_command
-)
 
 local custom_ivy = {
   theme = "ivy",
@@ -33,55 +22,31 @@ telescope.setup {
       prompt_position = "top",
       width = 0.90
     },
-    vimgrep_arguments = updated_vimgrep_args,
+
+    -- Extend default grep args!
+    vimgrep_arguments = vim.tbl_deep_extend('force',
+      { unpack(require("telescope.config").values.vimgrep_arguments) },
+      { "rg", "--hidden", "--glob", "!.git/*" }
+    ),
 
     mappings = {
       i = {
         ["<C-i>"] = actions.cycle_history_next,
         ["<C-o>"] = actions.cycle_history_prev,
-
-        ["<M-q>"] = actions.send_to_qflist + actions.open_qflist,
-
         ["<Esc>"] = actions.close,
         ["<C-w>"] = actions.close,
         ["<C-q>"] = actions.close,
-
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-
-        ["<CR>"] = actions.select_default,
         ["<C-h>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
-
-        ["<C-u>"] = actions.results_scrolling_up,
-        ["<C-d>"] = actions.results_scrolling_down,
-
         ["<Tab>"] = actions.move_selection_next,
         ["<S-Tab>"] = actions.move_selection_previous,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
       },
-      n = {
-        ["<M-q>"] = actions.send_to_qflist + actions.open_qflist,
 
-        ["<Esc>"] = actions.close,
+      n = {
         ["<C-w>"] = actions.close,
         ["<C-q>"] = actions.close,
-
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-
-        ["<CR>"] = actions.select_default,
         ["<C-h>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
-
-        ["<C-u>"] = actions.results_scrolling_up,
-        ["<C-d>"] = actions.results_scrolling_down,
-
         ["<Tab>"] = actions.move_selection_next,
         ["<S-Tab>"] = actions.move_selection_previous,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
       },
     },
   },
@@ -96,7 +61,7 @@ telescope.setup {
         end
       }
     },
-    find_files = { find_command = find_files_command },
+    find_files = { find_command = { "rg", "--hidden", "--files", "--glob", "!.git" } },
     lsp_definitions = custom_ivy,
     lsp_implementations = custom_ivy,
     lsp_references = custom_ivy
