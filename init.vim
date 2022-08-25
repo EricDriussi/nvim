c2cfc37a9960ecbf2944f022a9a1cd6fd757731d
Eric Driussi <ericdriussi@gmail.com>
2022-08-25 17:36:49 +0100

    refactor aucmds

--- init.vim
+++ init.vim
@@ -2,29 +2,16 @@ let mapleader = " "
 "Plug and generic Plugin settings
 source ~/.config/nvim/vimscript/plug.vim
 "Script to view a diff of different files given a vsplit
+"TODO.vim->lua
 source ~/.config/nvim/vimscript/diffmode.vim
 "Script to toggle an IDE like terminal
+"TODO.rm?
 source ~/.config/nvim/vimscript/terminal.vim
 "Autosave Sessions (DEPENDS ON notjedi/nvim-rooter.lua)
+"TODO.vim->lua && refactor
 source ~/.config/nvim/vimscript/sessions.vim
 
 lua require('config')
-
-"Always check if
-autocmd BufEnter * :call SetFiletypeNewBuffer()
-"Undefined buffer
-function! SetFiletypeNewBuffer()
-  if @% == ""
-    "And set filetype to none
-    :set filetype=none
-  endif
-endfunction
-"Enable spelling for commits and undefined buffers
-autocmd FileType gitcommit,none setlocal spell spelllang=es,en
-autocmd FileType man,harpoon,TelescopePrompt setlocal nospell
-
-"Don't bring comment to new line
-autocmd BufWinEnter * set formatoptions-=cro
 
 "Exit Vim meme here
 :command! WQ wq
--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,50 +1,85 @@
 local new_au_cmd = vim.api.nvim_create_autocmd
 local new_au_grp = vim.api.nvim_create_augroup
 
-local yank_group = new_au_grp('HighlightYank', {})
-new_au_cmd('TextYankPost', {
-  group = yank_group,
+new_au_cmd("BufWinEnter", {
+  desc = "Avoid autocomment on enter in commented line",
+  command = "set formatoptions-=cro"
+})
+
+new_au_cmd("TextYankPost", {
+  desc = "Highlight on yank",
+  group = new_au_grp("HighlightYank", {}),
   callback = function()
     vim.highlight.on_yank({
-      higroup = 'TermCursor',
+      higroup = "TermCursor",
       timeout = 60,
     })
   end,
 })
 
-local help_group = new_au_grp('HelpNav', {})
-new_au_cmd('FileType', {
-  group = help_group,
-  pattern = 'help',
+local ftp_group = new_au_grp("FileTypeSettings", {})
+new_au_cmd("FileType", {
+  desc = "Disable spellcheck for certain buffers",
+  group = ftp_group,
+  pattern = { "man", "harpoon", "TelescopePrompt" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
+
+new_au_cmd("BufEnter", {
+  desc = "Set filetype to none",
+  group = ftp_group,
+  callback = function()
+    if vim.bo.filetype == "" then
+      vim.opt_local.filetype = "none"
+    end
+  end,
+})
+
+new_au_cmd("FileType", {
+  desc = "Enable bilingual spellcheck for certain buffers",
+  group = ftp_group,
+  pattern = { "gitcommit", "none" },
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = { "es", "en" }
+  end,
+})
+
+new_au_cmd("FileType", {
+  desc = "GoTo navigation for help files",
+  group = ftp_group,
+  pattern = "help",
   callback = function()
     vim.keymap.set("n", "gd", "<C-]>")
   end,
 })
 
-local lsp_highlight_group = new_au_grp('LspHighlight', {})
+new_au_cmd("BufWritePre", {
+  desc = "Format on save",
+  group = new_au_grp("FormatOnSaveGroup", {}),
+  -- writing this in lua fks up the toggle
+  command = 'lua vim.lsp.buf.formatting_sync()'
+})
+
 Module = {}
+local lsp_highlight_group = new_au_grp("LspHighlight", {})
 function Module.activate_lsp_highlight()
-  new_au_cmd('CursorHold, CursorHoldI', {
+  new_au_cmd("CursorHold, CursorHoldI", {
     group = lsp_highlight_group,
-    pattern = '<buffer>',
+    pattern = "<buffer>",
     callback = function()
       vim.lsp.buf.document_highlight()
     end,
   })
-  new_au_cmd('CursorMoved, CursorMovedI', {
+
+  new_au_cmd("CursorMoved, CursorMovedI", {
     group = lsp_highlight_group,
-    pattern = '<buffer>',
+    pattern = "<buffer>",
     callback = function()
       vim.lsp.buf.clear_references()
     end,
-  })
-end
-
-function Module.create_format_on_save_cmd()
-  return new_au_cmd('BufWritePre', {
-    callback = function()
-      vim.lsp.buf.formatting_sync()
-    end
   })
 end
 
