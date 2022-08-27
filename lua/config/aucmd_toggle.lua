6d940a155ba59325c2fc5a5c537646706842600a
Eric Driussi <ericdriussi@gmail.com>
2022-08-27 19:21:07 +0100

    generalize toggle and handle by events

--- lua/config/aucmd_toggle.lua
+++ lua/config/aucmd_toggle.lua
@@ -1,17 +1,19 @@
-local aucmds = {}
-
 -- assumes the aucmds already exist
 -- TODO.accept callbacks in aucmd, nvim_get_autocmds only handles commands
--- TODO.refactor
+-- TODO.refactor?
+
+local aucmds_in_group = {}
 vim.api.nvim_create_user_command(
   "ToggleAucmdsByGroup",
   function(input)
-    local augroup_to_toggle = input.args
-    local aucmds_in_group = vim.api.nvim_get_autocmds({ group = augroup_to_toggle })
+    local given_group = input.args
+    local existing_aucmds = vim.api.nvim_get_autocmds({ group = given_group })
+    if aucmds_in_group[given_group] == nil then
+      aucmds_in_group[given_group] = {}
+    end
 
-    -- no aucmds found in group
-    if next(aucmds_in_group) == nil then
-      for _, cmd in pairs(aucmds) do
+    if next(existing_aucmds) == nil then
+      for _, cmd in pairs(aucmds_in_group[given_group]) do
         vim.api.nvim_create_autocmd(cmd.event, {
           desc = cmd.desc,
           group = cmd.group_name,
@@ -19,19 +21,54 @@ vim.api.nvim_create_user_command(
         })
         print(cmd.desc .. " turned ON")
       end
-      aucmds = {}
+      aucmds_in_group[given_group] = {}
 
-      -- aucmd founds
     else
-      for _, cmd in pairs(aucmds_in_group) do
-        table.insert(aucmds, cmd)
+      for _, cmd in pairs(existing_aucmds) do
+        table.insert(aucmds_in_group[given_group], cmd)
+        vim.api.nvim_del_autocmd(cmd.id)
+        print(cmd.desc .. " turned OFF")
+      end
+    end
+
+  end,
+  {
+    desc = "Global toggle for given augroup",
+    nargs = 1
+  }
+)
+
+local aucmds_for_event = {}
+vim.api.nvim_create_user_command(
+  "ToggleAucmdsByEvent",
+  function(input)
+    local given_event = input.args
+    local existing_aucmds = vim.api.nvim_get_autocmds({ group = given_event })
+    if aucmds_for_event[given_event] == nil then
+      aucmds_for_event[given_event] = {}
+    end
+
+    if next(existing_aucmds) == nil then
+      for _, cmd in pairs(aucmds_for_event[given_event]) do
+        vim.api.nvim_create_autocmd(cmd.event, {
+          desc = cmd.desc,
+          group = cmd.group_name,
+          command = cmd.command
+        })
+        print(cmd.desc .. " turned ON")
+      end
+      aucmds_for_event[given_event] = {}
+
+    else
+      for _, cmd in pairs(existing_aucmds) do
+        table.insert(aucmds_for_event[given_event], cmd)
         vim.api.nvim_del_autocmd(cmd.id)
         print(cmd.desc .. " turned OFF")
       end
     end
   end,
   {
-    desc = "Global toggle for given aucmd",
+    desc = "Global toggle for aucmds for given event",
     nargs = 1
   }
 )
