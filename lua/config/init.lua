db059dd1b7da23c14df16aebf77cd337cfa7d458
Eric Driussi <ericdriussi@gmail.com>
2022-08-27 17:38:08 +0100

    add diagnostics config

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,5 +1,7 @@
 -- Settings/Options
 require("config.setopts")
+-- Diagnostics
+require("config.diagnostics")
 -- Autocmds
 require("config.autocommands")
 -- Basic Keymap
@@ -13,34 +15,4 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
   border = "rounded",
-})
-
--- TODO.refactor
-local signs = {
-  { name = "DiagnosticSignError", text = "" },
-  { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignHint", text = "" },
-  { name = "DiagnosticSignInfo", text = "" },
-}
-
-for _, sign in ipairs(signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
-vim.diagnostic.config({
-  virtual_text = false,
-  signs = {
-    active = signs,
-  },
-  update_in_insert = true,
-  underline = true,
-  severity_sort = true,
-  float = {
-    focusable = false,
-    style = "minimal",
-    border = "rounded",
-    source = "always",
-    header = "",
-    prefix = "",
-  },
 })
