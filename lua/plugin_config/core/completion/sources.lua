7f633860a0223f618232f331bb7eb8c7cdd0c461
Eric Driussi <ericdriussi@gmail.com>
2022-10-28 17:15:16 +0100

    cmp and gofumpt

--- lua/plugin_config/core/completion/sources.lua
+++ lua/plugin_config/core/completion/sources.lua
@@ -1,9 +1,9 @@
 return {
-  { name = "nvim_lsp", prority = 9 },
-  { name = "luasnip", max_item_count = 3 },
-  { name = "cmp_tabnine" },
-  { name = "buffer", keyword_length = 4, max_item_count = 3 },
-  { name = 'emoji', max_item_count = 5 },
-  { name = "path" },
-  { name = "nvim_lsp_signature_help" },
+	{ name = "nvim_lsp" },
+	{ name = "luasnip", max_item_count = 3 },
+	{ name = "cmp_tabnine" },
+	{ name = "buffer", keyword_length = 5, max_item_count = 3 },
+	{ name = "emoji", max_item_count = 5 },
+	{ name = "path" },
+	{ name = "nvim_lsp_signature_help" },
 }
--- lua/plugin_config/core/null-ls.lua
+++ lua/plugin_config/core/null-ls.lua
@@ -4,32 +4,34 @@ local diagnostics = null_ls.builtins.diagnostics
 local actions = null_ls.builtins.code_actions
 
 null_ls.setup({
-  debug = false,
-  sources = {
-    formatting.eslint_d.with({
-      timeout = 10000,
-    }),
+	debug = false,
+	sources = {
+		formatting.eslint_d.with({
+			timeout = 10000,
+		}),
 
-    --diagnostics.yamllint,
-    diagnostics.markdownlint.with({
-      extra_args = { "--disable", "MD029", "MD013", "MD041", "MD024", "MD025" }
-    }),
-    formatting.markdownlint,
+		--diagnostics.yamllint,
+		diagnostics.markdownlint.with({
+			extra_args = { "--disable", "MD029", "MD013", "MD041", "MD024", "MD025" }
+		}),
+		formatting.markdownlint,
 
-    actions.shellcheck,
-    diagnostics.shellcheck,
-    formatting.shfmt,
+		actions.shellcheck,
+		diagnostics.shellcheck,
+		formatting.shfmt,
 
-    diagnostics.zsh,
-    formatting.beautysh.with({
-      filetypes = { "zsh" }
-    }),
+		diagnostics.zsh,
+		formatting.beautysh.with({
+			filetypes = { "zsh" }
+		}),
 
-    formatting.autopep8.with({
-      extra_args = { "-a" }
-    }),
-    diagnostics.mypy.with({
-      extra_args = { "--config-file", vim.fn.getcwd() .. "/setup.cfg" }
-    }),
-  },
+		formatting.autopep8.with({
+			extra_args = { "-a" }
+		}),
+		diagnostics.mypy.with({
+			extra_args = { "--config-file", vim.fn.getcwd() .. "/setup.cfg" }
+		}),
+
+		formatting.gofumpt,
+	},
 })
