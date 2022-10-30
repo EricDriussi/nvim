2aa9c1729b084082115290dd85074d598447251b
Eric Driussi <ericdriussi@gmail.com>
2022-10-30 18:11:12 +0000

    bold word under cursor

--- lua/plugin_config/colorscheme.lua
+++ lua/plugin_config/colorscheme.lua
@@ -4,8 +4,8 @@ vim.api.nvim_command "colorscheme catppuccin"
 
 -- Highlight references for word under cursor
 local cp = require("catppuccin.palettes").get_palette("mocha")
-vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = cp.surface0 })
-vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = cp.surface0 })
+vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = cp.surface0, bold = true })
+vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = cp.surface0, bold = true })
 vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = cp.surface0, bold = true })
 
 -- Make visual selections more visible
