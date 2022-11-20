6003a0562c481e976148409fc7f50d41a9601161
Eric Driussi <ericdriussi@gmail.com>
2022-11-20 09:05:25 +0000

    improve big-init

--- big-init.lua
+++ big-init.lua
@@ -1,14 +1,49 @@
-local option = vim.opt
-option.cursorline = false
-option.cursorcolumn = false
-option.relativenumber = false
-option.signcolumn = false
-option.hidden = false
-
-vim.api.nvim_command("syntax sync minlines=200")
-vim.api.nvim_command("syntax sync maxlines=500")
+require("config")
 
 vim.api.nvim_command("command! WQ wq")
 vim.api.nvim_command("command! Wq wq")
 vim.api.nvim_command("command! W w")
 vim.api.nvim_command("command! Q q")
+
+-- Perf settings
+local option = vim.opt
+option.cursorline = false
+option.cursorcolumn = false
+option.relativenumber = false
+option.signcolumn = "no"
+option.hidden = false
+option.syntax = "off"
+option.lazyretraw = true
+
+-- Disable builtin plugins
+local disabled_built_ins = {
+  "2html_plugin",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "matchit",
+  "tar",
+  "tarPlugin",
+  "rrhelper",
+  "spellfile_plugin",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin",
+  "tutor",
+  "rplugin",
+  "synmenu",
+  "optwin",
+  "compiler",
+  "bugreport",
+  "ftplugin",
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+  vim.g["loaded_" .. plugin] = 1
+end
