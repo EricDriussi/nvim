54ed219635f43b840898afc9734f89cef6479870
Eric Driussi <ericdriussi@gmail.com>
2022-12-16 15:17:22 +0000

    theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
-vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_background = "medium"
 vim.g.gruvbox_material_foreground = "mix"
 vim.g.gruvbox_material_better_performance = 1
 vim.g.gruvbox_material_enable_italic = 1
