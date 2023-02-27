65cdbe870590e48a450ea641fb1a29360bf549d9
Eric Driussi <ericdriussi@gmail.com>
2023-02-27 14:35:03 +0000

    add firenvim plugin

--- lua/plug.lua
+++ lua/plug.lua
@@ -1,8 +1,8 @@
 local list = require("plug_list")
 local plugins = {
 	-- Needed for other plugins
-	{ "nvim-lua/popup.nvim", priority = 1000 },
-	{ "nvim-lua/plenary.nvim", priority = 1000 },
+	{ "nvim-lua/popup.nvim",          priority = 1000 },
+	{ "nvim-lua/plenary.nvim",        priority = 1000 },
 	{ "kyazdani42/nvim-web-devicons", priority = 1000 },
 	list.UI,
 	list.file_viewers,
@@ -14,6 +14,24 @@ local plugins = {
 	list.cmp,
 	{ "EricDriussi/aucmd-toggle.nvim" },
 	{ "EricDriussi/remember-me.nvim", config = true },
+
+	{
+		"glacambre/firenvim", -- from https://github.com/glacambre/firenvim#installing
+		cond = not not vim.g.started_by_firenvim,
+		build = function()
+			require("lazy").load({ plugins = "firenvim", wait = true })
+			vim.fn["firenvim#install"](0)
+		end,
+		init = function()
+			vim.g.firenvim_config = {
+				localSettings = {
+					[".*excalidraw.*"] = { takeover = "never" },
+					[".*notion.*"] = { takeover = "never" },
+					[".*docs.google.*"] = { takeover = "never" },
+				},
+			}
+		end,
+	},
 }
 
 local setup = {
