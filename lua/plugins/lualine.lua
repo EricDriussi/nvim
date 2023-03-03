a72717dd64eb381f40dfe05fa8183d9d52f4bce0
Eric Driussi <ericdriussi@gmail.com>
2023-03-03 20:50:13 +0000

    add reading time to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,14 @@
+local function readingTime()
+	local isMarkdown = vim.bo.filetype == "markdown"
+	if isMarkdown then
+		return string.format("%s 🕐", math.ceil(vim.fn.wordcount().words / 250))
+	end
+	return ""
+end
+
 return {
 	options = {
-		component_separators = { left = "", right = "" },
+		component_separators = { left = "|", right = "|" },
 		section_separators = { left = "", right = "" },
 		theme = "gruvbox-material",
 	},
@@ -8,7 +16,7 @@ return {
 		lualine_a = { "mode" },
 		lualine_b = { "branch" },
 		lualine_c = { "diagnostics" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_x = { "fileformat", readingTime, "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
 	},
