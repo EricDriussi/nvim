66736952a7f6535c31ed824b18ad6b33a533a474
Eric Driussi <ericdriussi@gmail.com>
2023-03-31 13:57:21 +0100

    formatting

--- lua/plugins/completion/mappings.lua
+++ lua/plugins/completion/mappings.lua
@@ -9,20 +9,16 @@ end
 return {
 	-- trigger completion
 	["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-
 	-- accept currently selected item. If none selected, `select = true` first item.
 	["<CR>"] = cmp.mapping.confirm({ select = true }),
-
 	-- abort completion (doesn't behave as expected with <esc>)
 	["<C-q>"] = cmp.mapping(cmp.mapping.abort()),
-
 	-- scroll completions
 	["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
 	["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 	-- scroll docs
-	["<C-k>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+	["<C-k>"] = cmp.mapping(cmp.mapping.scroll_docs( -1), { "i", "c" }),
 	["<C-j>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-
 	-- Super tab!!
 	["<Tab>"] = cmp.mapping(function(fallback)
 		if cmp.visible() then
@@ -35,12 +31,11 @@ return {
 			fallback()
 		end
 	end, { "i", "s" }),
-
 	["<S-Tab>"] = cmp.mapping(function(fallback)
 		if cmp.visible() then
 			cmp.select_prev_item()
-		elseif luasnip.jumpable(-1) then
-			luasnip.jump(-1)
+		elseif luasnip.jumpable( -1) then
+			luasnip.jump( -1)
 		else
 			fallback()
 		end
