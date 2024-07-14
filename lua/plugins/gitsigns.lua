90c44591188597dd23f9fdde06639ae9b3663233
Eric Driussi <ericdriussi@gmail.com>
2024-07-14 20:01:51 +0100

    update gitsigns conf

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,25 +2,12 @@ return {
 	"lewis6991/gitsigns.nvim",
 	opts = {
 		signs = {
-			add = { hl = "GitSignsAdd", text = " ▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-			change = { hl = "GitSignsChange", text = " ▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-			delete = { hl = "GitSignsDelete", text = " ▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-			topdelete = {
-				hl = "GitSignsDelete",
-				text = " ▎",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			changedelete = {
-				hl = "GitSignsChange",
-				text = " ▎",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
+			add = { text = " ▎" },
+			change = { text = " ▎" },
+			delete = { text = " ▎" },
+			topdelete = { text = " ▎" },
+			changedelete = { text = " ▎" },
 			untracked = { text = " ┆" },
-		},
-		current_line_blame_formatter_opts = {
-			relative_time = true,
 		},
 	},
 }
