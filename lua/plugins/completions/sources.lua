48aa4a4431216d92843e3dcfae12bd5f7f53b421
Eric Driussi <ericdriussi@gmail.com>
2024-08-02 18:26:54 +0100

    init lazyvim

--- lua/plugins/completions/sources.lua
+++
@@ -1,8 +0,0 @@
-return {
-	{ name = "nvim_lsp" },
-	{ name = "nvim_lsp_signature_help" },
-	{ name = "luasnip", max_item_count = 2 },
-	{ name = "buffer", max_item_count = 4 },
-	{ name = "emoji" },
-	{ name = "path" },
-}
--- lua/plugins/lsp/lang_settings/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-	lua_ls = require("plugins.lsp.lang_settings.lua_ls"),
-	jsonls = require("plugins.lsp.lang_settings.jsonls"),
-	ansiblels = require("plugins.lsp.lang_settings.ansiblels"),
-}
