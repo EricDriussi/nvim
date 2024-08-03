673ac8ddd1e989ddc05ab0a182a62d90792103df
Eric Driussi <ericdriussi@gmail.com>
2024-08-03 15:35:24 +0100

    aucmds keymap and opts

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,55 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+local aucmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+aucmd("BufWinEnter", {
+  desc = "Avoid autocomment on 'o' in commented line",
+  command = "set formatoptions-=o",
+})
+
+-- rm LazyVim autocmd
+local yank_event = "TextYankPost"
+vim.api.nvim_clear_autocmds({
+  event = yank_event,
+})
+
+aucmd(yank_event, {
+  desc = "Highlight on yank",
+  group = augroup("HighlightYank", {}),
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "TermCursor",
+      timeout = 60,
+    })
+  end,
+})
+
+local ftp_group = augroup("FileTypeSettings", {})
+aucmd("FileType", {
+  desc = "Disable spellcheck for certain buffers",
+  group = ftp_group,
+  pattern = { "man", "TelescopePrompt" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
+
+aucmd("FileType", {
+  desc = "GoTo navigation for help files",
+  group = ftp_group,
+  pattern = { "help", "man" },
+  callback = function()
+    vim.keymap.set("n", "gd", "<C-]>")
+  end,
+})
+
+-- TODO: I think LV already does this?
+-- aucmd("VimEnter", {
+--   desc = "Set root dir",
+--   group = augroup("SetRootOnVimEnter", {}),
+--   callback = function(ctx)
+--     local root = vim.fs.root(ctx.buf, { ".git" })
+--     if root then
+--       vim.uv.chdir(root)
+--     end
+--   end,
+-- })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,17 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.g.lazyvim_picker = "telescope"
+vim.g.lazyvim_statuscolumn = {
+  folds_open = true,
+}
+
+local opt = vim.opt
+
+opt.clipboard = ""
+opt.conceallevel = 0
+opt.scrolloff = 8
+opt.sidescrolloff = 5
+opt.spelllang = { "en" }
+opt.spelloptions:append("camel")
+opt.undolevels = 10000
+opt.updatetime = 200
+opt.undodir = vim.env.HOME .. "/.config/nvim/.undodir/"
+opt.wrap = true
